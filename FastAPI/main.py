8e16fc84950eaeb25f414181be0544f9aee57ded
Chirag Debnath <119398407+itsChiragg@users.noreply.github.com>
2024-08-09 21:53:22 +0530

    project upload

---
+++ FastAPI/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+# from routes.entry import entry_root
+from routes.blog import blog_root
+from routes.user import user_root
+from routes.comment import comment_root
+
+
+
+app = FastAPI()
+
+# app.include_router(entry_root)
+app.include_router(blog_root)
+app.include_router(user_root)
+app.include_router(comment_root)
